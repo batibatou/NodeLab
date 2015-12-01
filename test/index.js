9eee6b5639a8863d4569fb0b2a5247f18c143758
batibatou <batou.pandaria@gmail.com>
2015-12-01 17:06:14 +0100

    ajout chai

--- test/index.js
+++ test/index.js
@@ -1,8 +1,16 @@
 import assert from 'assert';
+import { should } from 'chai';
 import NodeLab from '../lib';
 
 describe('NodeLab', function () {
   it('should have a version number!', function () {
     assert(typeof NodeLab.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
+  });
+});
+should();
+
+describe('NodeLab', function () {
+  it('should have a version number!', function () {
+    NodeLab.should.have.property('VERSION');
   });
 });
